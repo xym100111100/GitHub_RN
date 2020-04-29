e2bbb3739b3fb87aebba9a833977bcebc1ca4125
亚明 <253274870@qq.com>
2020-04-29 09:12:32 +0800

    初始化

---
+++ index.js
@@ -0,0 +1,11 @@
+/**
+ * @format
+ */
+import 'react-native-gesture-handler';
+
+import { AppRegistry } from 'react-native';
+import App from './App';
+import WelcomePage from "./js/page/WelcomePage"
+import { name as appName } from './app.json';
+
+AppRegistry.registerComponent(appName, () => WelcomePage);
---
+++ js/page/WelcomePage.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react"
+import { StyleSheet, View, Text } from "react-native"
+import Ionicons from 'react-native-vector-icons/Ionicons'
+
+export default class WelcomePage extends Component {
+    componentDidMount() {
+        this.timer = setTimeout(() => {
+            // 跳转到首页
+        }, 2000);
+    }
+
+    componentWillMount() {
+        // 页面销毁的时候清空定时器，与启动定时器成对出现！
+        this.timer && clearTimeout(this.timer)
+    }
+
+    render() {
+        return (
+            <View style={style.container} >
+                <Text>欢迎
+                    <Ionicons
+                        name={'ios-home'}
+                        size={26}
+                        style={{ color: 'red' }}
+                    />页面</Text>
+            </View>
+        )
+    }
+}
+
+const style = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: 'center'
+    }
+})
